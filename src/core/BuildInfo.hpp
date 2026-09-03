940802f8ebb937dfef922d1e218feebc38c250fb
vaporfly0987 <eat_squat76@icloud.com>
2026-09-03 16:46:37 -0400

    Add ForgeSlice build information

---
+++ src/core/BuildInfo.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef FORGESLICE_VERSION
+#define FORGESLICE_VERSION "0.1.0"
+#endif
+
+namespace forgeslice::core {
+inline constexpr const char* kProductName = "ForgeSlice";
+inline constexpr const char* kVersion = FORGESLICE_VERSION;
+}
